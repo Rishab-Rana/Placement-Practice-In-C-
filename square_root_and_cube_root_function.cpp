0dce02209c59bffba102ea85bf29147be0f53657
Lucifer <rishabrana228@gmail.com>
2023-02-19 11:35:44 +0530

    learned about the square root and cube root function in c++ and cmath header file

---
+++ square_root_and_cube_root_function.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+int x=5;
+double square;
+double cube;
+square = sqrt(x);
+cout<<"the Square root of 5 is "<<square<<endl;
+cube = cbrt(x);
+cout<<"the cube root of 5 is "<<cube<<endl;
+    return 0;
+}
